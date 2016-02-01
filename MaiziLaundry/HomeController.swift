1ac0724a1cf18488f132e471f72456f9ac414190
alatan <althygmgl@gmail.com>
2016-02-01 16:07:57 +0800

    第一次提交代码

---
+++ MaiziLaundry/HomeController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeController.swift
+//  MaiziLaundry
+//
+//  Created by maiziedu on 1/29/16.
+//  Copyright © 2016 Alatan. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "麦子洗衣"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
